06edc3d7575c02feebfae55ea92f3322e133c4b6
Artem Ksenofontov <tema339517@gmail.com>
2025-03-03 19:18:28 +0300

    Добавил изменение размера печеньки

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,5 +5,11 @@ let clicks = 0;
 
 cookie.addEventListener("click", () => {
     clicks++;  
-    counter.textContent = clicks; 
+    counter.textContent = clicks;
+    
+    cookie.style.transform = "scale(1.2)";
+
+    setTimeout(() => {
+        cookie.style.transform = "scale(1)";
+    }, 100);
 });
